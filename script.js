885fd9378fcc0704acbca3630f4fc99ce27d7ac0
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-10-17 09:51:08 +0300

    ec gr 2

--- script.js
+++ script.js
@@ -280,7 +280,7 @@
 // for(let j=1;j>0;j--){
 //     console.log(j);
 // }
-let myArray=["Ana","Mihai","Alina","Cosmin"];
+// let myArray=["Ana","Mihai","Alina","Cosmin"];
 // for (let k=0;k<=myArray.length;k++)
 //     {
 //     console.log(myArray[k]);
@@ -289,38 +289,53 @@ let myArray=["Ana","Mihai","Alina","Cosmin"];
 //     {
 //     console.log(myArray[m]);
 //     }
-index=0;
-while(index<=myArray.length)
-{
-    console.log(myArray[index]);
-    index++;
-}
-console.log("Afisare array cu ajutorul do-while")
-let k=0
-do{
-console.log(myArray[k]);
-k++;
-}
-while (k<myArray.length)
- console.log("instructiunea brake ");
-for(let i=0;i<myArray.length;i++)
-{
-    console.log(myArray[i]);
-    if (myArray[i]==="Mihai"){
-        break; //iesim din instructiunea repetitiva
+// index=0;
+// while(index<=myArray.length)
+// {
+//     console.log(myArray[index]);
+//     index++;
+// }
+// console.log("Afisare array cu ajutorul do-while")
+// let k=0
+// do{
+// console.log(myArray[k]);
+// k++;
+// }
+// while (k<myArray.length)
+//  console.log("instructiunea brake ");
+// for(let i=0;i<myArray.length;i++)
+// {
+//     console.log(myArray[i]);
+//     if (myArray[i]==="Mihai"){
+//         break; //iesim din instructiunea repetitiva
 
-    }
-}
- console.log("instructiunea continue");
-for(let p=0;p<myArray.length;p++)
-{
+//     }
+// }
+//  console.log("instructiunea continue");
+// for(let p=0;p<myArray.length;p++)
+// {
    
-    if (myArray[p]==="Mihai"){
-        continue; //sare peste un element din array, face ca ceea ce este dupa
-        //sa nu se mai execute si forul trece la urmatorul element
+//     if (myArray[p]==="Mihai"){
+//         continue; //sare peste un element din array, face ca ceea ce este dupa
+//         //sa nu se mai execute si forul trece la urmatorul element
         
-    }
-      console.log(myArray[p]);
-}
+//     }
+//       console.log(myArray[p]);
+// }
+
+//ecuatie de gradul 2
+let a=2;
+let b=4;
+let c=2;
+let mesaj=`Ecuatia de gradul 2 de forma: ${a}*x^2+${b}*x+${c}=0`;
+console.log(mesaj);
+let delta=b*b-4*a*c;
+let sol1=(-b+Math.sqrt(delta))/(2*a);
+let sol2=(-b-Math.sqrt(delta))/(2*a);
+
+console.log(`Rezolvare: delta=${delta},x1=${sol1},x2=${sol2}`)
+
+
+
 
 //FUNCTII
