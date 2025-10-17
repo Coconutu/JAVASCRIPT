957c305c1baa717549cf7818b952b9ca21e4c1c6
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-10-17 14:03:30 +0300

    update

--- FUNCTII/script.js
+++ FUNCTII/script.js
@@ -1,4 +1,68 @@
-function myFunction(){ //semnatura functiei
-    console.log("S-a apelat functia myFunction"); //intre acolade corpul functiei
+function myFunction1(a,b){ //semnatura functiei
+   return a+b; //intre acolade corpul functiei
 }
+let firstNumber=4;
+let secondNumber=8;
+result=myFunction1(firstNumber,secondNumber);
+console.log(result);
+
+//functie anonima,careia ii putem schimba comportamentul
+let sum=function(a,b){ 
+    return a+b;
+}
+console.log(sum(4,10));
+
+sum=function(a,b){
+    return a*b;
+}
+console.log(sum(4,10));
+
+//arrow function
+let suma=(a,b)=>a+b; //daca avem doar o instructione, putem elimina parantezele
+console.log(suma(4,10));
+
+let person={
+    firstName:"Alex",
+    lastName:"Pop",
+    age:40
+};
+let person2={
+    firstName:"Mihai",
+    lastName:"Ionescu",
+    age:50
+};
+// console.log(`Nume complet:${person.firstName} ${person.lastName}`)
+// console.log(`Varsta:${person.age}`)
+// console.log(`Nume complet:${person2.firstName} ${person2.lastName}`)
+// console.log(`Varsta:${person2.age}`)
+
+function afiseaza(text){
+    console.log(text);
+}
+
+afiseaza("Apelare functie displayDetails");
+
+displayDetails(person);
+displayDetails(person2);
+
+function displayDetails(personObject){
+    console.log(`Nume complet:${personObject.firstName} ${personObject.lastName}`)
+    console.log(`Varsta:${personObject.age}`)
+    
+}
+
+let number=1;
+function myFunction(){
+    let rezultat=number+10;
+    afiseaza(`Valoarea pentru result este ${result}`);
+    }
 myFunction();
+afiseaza(rezultat); //not defined pentru ca incercam sa apelam o variabila care poate fi accesata doar in interiorul functiei
+//putem accesa variabilele din afara in functie, dar invers nu merge.
+//Variabila result este locala. number este globala, deoarece poate fi accesata de oriunde
+
+let value=1;
+function myFunction2(param){
+    param++;
+    afiseaza(param)
+}
---
+++ Variabile/script.js
@@ -0,0 +1,77 @@
+//alert("Acesta este codul meu de javascript");  
+//console.log("acesta este un mesaj");
+//console.warn("Atentie");
+//console.error("Eroare");
+//declaram variabila folosind var, let, const
+//let myNumber = 2; //declaram variabila si nu mai e nevoie sa o declaram din nou
+//console.log(myNumber);
+//myNumber = 10;
+//console.log(myNumber);
+//const age=10 //nu se poate modifica
+//console.log(age);
+//let message;
+//console.log(message); //va afisa undefined deoarece nu a fost atribuita nicio valoare dupa declaratie
+
+//Reguli de denumire
+//1. numele poate sa contuina doar litere, cifre, _ sau $
+//2. nu poate fi un cuvant cheie, nu are spatii, nu poate incepe cu o cifra,nu contine cuvinte cheie
+//3. numele trebuie sa fie cat mai sugestive
+//diferent dintre var si let este data de ciclul de viata al variabilei
+//var poate fi accesat si in exteriorul blocului, vizibil in tot fisierul de javascript (variabila globala)
+//let poate fi accesat doar in interiorul blocului, vizibile doar in bloc(variabila locala)
+//este de oreferat sa nu folosim var
+//{
+//    let firstName="Marius"; 
+//    console.log(firstName);
+
+//}
+//console.log(firstName);
+
+//TIPURI DE DATE PRIMITIVE
+
+//boolean - stocheaza true sau false
+// let isActive = true;
+// console.log(typeof isActive);
+//number
+// let myNumber = 10.3;
+// console.log(typeof myNumber);
+//big int - mai rar intalnite pt a stoca numere foarte mari-unde nu poate fi folosit number
+//symbol - mai rar pt a crea valori unice
+//string - sir de caractere
+// let fullName="Rares Petrisor";
+// console.log(typeof fullName);
+//null
+// let myNull=null;
+// console.log(typeof myNull);
+//undefined
+// let myUndefined;
+// console.log(typeof myUndefined);
+
+
+//TIPURI DE DATE REFERINTA  
+//     //ARRAY-URI
+//     //OBIECTE - COLECTIE DE PROPRIETATI 
+
+
+//     let person={
+//         firstName:"Alex",
+//         lastName:"Pop",
+//         age:30
+//     };
+// console.log(typeof person);
+
+//         //FUNCTII
+//     //DATE CALENDARISTICE
+//     //EXPRESII REGEX- TOPIC MAI COMPLEX
+// let firstNumber={value:1};
+// let secondNumber=firstNumber;
+// console.log(firstNumber);
+// console.log(secondNumber);
+
+// secondNumber.value=10;
+// console.log(firstNumber);
+// console.log(secondNumber);
+// firstNumber=10;
+// secondNumber=firstNumber;
+// console.log(firstNumber);
+// console.log(secondNumber);
