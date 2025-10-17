3e2dfae0313a2d977776d1da6802aca19b91f096
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-10-17 13:13:24 +0300

    modificari structurale

--- CURS INTRO/script.js
+++ CURS INTRO/script.js
@@ -324,18 +324,17 @@
 // }
 
 //ecuatie de gradul 2
-let a=2;
-let b=4;
-let c=2;
-let mesaj=`Ecuatia de gradul 2 de forma: ${a}*x^2+${b}*x+${c}=0`;
-console.log(mesaj);
-let delta=b*b-4*a*c;
-let sol1=(-b+Math.sqrt(delta))/(2*a);
-let sol2=(-b-Math.sqrt(delta))/(2*a);
+// let a=2;
+// let b=4;
+// let c=2;
+// let mesaj=`Ecuatia de gradul 2 de forma: ${a}*x^2+${b}*x+${c}=0`;
+// console.log(mesaj);
+// let delta=b*b-4*a*c;
+// let sol1=(-b+Math.sqrt(delta))/(2*a);
+// let sol2=(-b-Math.sqrt(delta))/(2*a);
 
-console.log(`Rezolvare: delta=${delta},x1=${sol1},x2=${sol2}`)
-
-
-
+// console.log(`Rezolvare: delta=${delta},x1=${sol1},x2=${sol2}`)
 
 //FUNCTII
+
+
