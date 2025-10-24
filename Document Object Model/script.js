24668ab725eaa925da457cb7dd20a05ecb7dab66
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-10-24 09:12:23 +0300

    selectare elemente din html

---
+++ Document Object Model/script.js
@@ -0,0 +1,6 @@
+const mainTitle=document.getElementById("mainTitle");
+console.log(mainTitle);
+const titles=document.getElementsByClassName("title");
+console.log(titles);
+const items=document.getElementsByTagName("li");
+console.log(items);
