89d32fca5da974f2ad4beba1c95745df87e3095e
Daniel Sebastian <sebastian.daniel.adrian@gmail.com>
2025-10-28 13:08:50 +0200

    update

--- Proiect To-do list/script.js
+++ Proiect To-do list/script.js
@@ -1,19 +1,20 @@
 const taskForm = document.querySelector("#taskForm");
 taskForm.addEventListener("submit", onTaskFormSubmit);
 function onTaskFormSubmit(event) {
-  event.preventDefault();
-  const tasklist = document.querySelector(".tasks");
-  const taskinput = document.querySelector("input");
-  if (!taskinput.value) {
-    alert("Completeaza denumirea!");
-  }
-  const newtask=document.createElement("li");
-  newtask.innerText=taskinput.value;
-  newtask.classList.add("tasks");
-  tasklist.append(newtask);
-  newtask.addEventListener("click",()=>{
-    newtask.remove();
-  })
+    event.preventDefault();
+    const tasklist = document.querySelector(".tasks");
+    const taskinput = document.querySelector("input");
+    if (!taskinput.value) {
+        alert("Completeaza denumirea!");
+        return;
+    }
+    const newTask = document.createElement("li");
+    newTask.innerText = taskinput.value;
+    newTask.classList.add("tasks");
+    tasklist.append(newTask);
 
-  
+    newTask.addEventListener("click", () => {
+        newTask.remove();
+    })       
+taskinput.value = "";
 }
