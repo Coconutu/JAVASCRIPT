21167514dab8ff451e0faf5642d28de4a4a8e808
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-11-07 09:16:27 +0200

    work

--- Math/script.js
+++ Math/script.js
@@ -1,25 +1,37 @@
-const calcForm=document.querySelector('#calcForm');
-calcForm.addEventListener('submit',verificaRezultat);
-function verificaRezultat(event){
-    event.preventDefault(); 
-    let a=Math.floor(Math.random() * 101);
-    let b=Math.floor(Math.random() * 101);   
-    const intrebare=document.createElement('h1')
-    intrebare.innerText=`Cat face ${a} + ${b} ?`;
-    const textcalcule=document.querySelector('#textcalcule');
-    textcalcule.innerHTML='';
-    textcalcule.appendChild(intrebare); 
-
-    const rezultat=document.querySelector('#rezultat');
-    if(!rezultat.value){
-        alert('Te rugam sa introduci un rezultat!');
-        return;
-    }
-    const userRezultat=parseInt(rezultat.value);
-    const corectRezultat=a+b;
-    
-
+const valoareDificultate = document.querySelector('#dificultate'); //dificultate.value=100
+valoareDificultate.addEventListener('change', () => {
+    rezultatCorect = genereazaIntrebare();
+});
+function genereazaIntrebare() {
+    let a = Math.floor(Math.random() * valoareDificultate.value);
+    let b = Math.floor(Math.random() * valoareDificultate.value);
+    const intrebare = document.querySelector('#intrebare');
+    intrebare.innerText = `Cat face ${a} + ${b} ?`;
+    return a + b;
 }
+let rezultatCorect = genereazaIntrebare();
 
+const calculForm = document.querySelector("#calcForm");
+calculForm.addEventListener("submit", verificaRezultat);
 
+function verificaRezultat(event) {
+   event.preventDefault();
+   const userRezultat = document.querySelector("#userRezultat");
+   if (!userRezultat.value) {
+       alert('Te rugam sa introduci un rezultat!');
+       return;
+   }
+   else if (userRezultat.value == rezultatCorect) {
+       alert('Raspuns corect! Felicitari!');
+       
+   }
+   else{
+    alert('Raspuns gresit! Mai incearca!');
 
+   }
+
+   console.log(`rezultatCorect: ${rezultatCorect}`);
+   console.log(`userRezultat: ${userRezultat.value}`);
+    userRezultat.value = '';
+   rezultatCorect = genereazaIntrebare();
+}
